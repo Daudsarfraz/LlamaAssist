24246d6edb79fdffb2fc7dcc7d89edc29f14ebc8
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2024-12-08 16:04:03 +0500

    update

--- save_to_file/save_to_file.py
+++ save_to_file/save_to_file.py
@@ -1,9 +1,27 @@
 import os
 def create_file(file_name):
-    search_file = os.listdir(file_name)
-    print(search_file)
+
+    try:
+        file_found = [i for i in os.listdir(os.getcwd()) if i==file_name]
+        if file_found:
+            print("Entered File Found", file_name)
+        else:
+             with open(file_name, "w") as f:
+                  print(f"File {file_name} has been created")
 
 
 
-create_file("words_count.txt")
+    except Exception as e:
+            print(f"{e}")
+        # with open(file_name, "a") as f:
+        #     print("File is Created", file_name)
+
+        #     f.close()
+
+
+
+
+
+file_name = "words_count12.txt"
+create_file(file_name)
 
